51b002dfb346cc016c440aa8acb90170ac6cb561
BazylevDV <dmbazylev@gmail.com>
2024-07-05 14:48:22 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+homeworks=12# количество выполненных дз
+hours=1.5 # количество затраченных часов
+course="Python" # название курса
+All_time=(1.5/12) # время на одно задание
+print("Курс:", course,",", "всего задач :",homeworks,",""затрачено часов:", hours,",""среднее время выполнения",All_time ,"часа.")
+
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
